d06d04ba47ed0c88aa5c0740df70fb61a2341ab3
arielluispe <79119501+arielluispe@users.noreply.github.com>
2021-03-22 01:09:16 +0100

    Add files via upload

---
+++ 1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        
+
+    class persona
+    {
+            private int cedula;
+            private String nombre;
+            private String apellido;
+            private int edad;
+
+            public persona()
+            {
+                cedula = 402156848;
+                nombre = "Ariel";
+                apellido = "Martinez";
+                edad = 18;
+            }
+
+         
+            public String responsabilidad(){
+
+               return "\n Los datos de la persona son:\n "+cedula+" "+nombre+" "+apellido+" "+edad+"\n";
+            
+            }
+
+        }
+
+       class profesor : persona{
+
+            public profesor()
+            {
+                sueldo = 60000;
+                
+            }
+
+
+            private int sueldo;           
+           
+            public String getsueldo()
+            {
+              return "El sueldo del profesor es: "+ sueldo;
+            }
+
+            static void Main(string[] args)
+        {
+            persona a = new persona();
+            Console.WriteLine(a.responsabilidad());
+
+            profesor b = new profesor();
+            Console.WriteLine(b.getsueldo());
+            Console.ReadLine();
+                }
+
+
+        }
+    }
+
+}
---
+++ 2.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    class Contacto
+    {
+        private string nombre;
+        private string apellidos;
+        private string telefono;
+        private string direccion;
+
+        public void SetContacto()
+        {
+
+            Console.WriteLine("¿Cual es su nombre? ");
+            nombre=Console.ReadLine();
+
+            Console.WriteLine("¿Cual es su apellido? ");
+            apellidos=Console.ReadLine();
+
+            Console.WriteLine("¿Cual es su Telefono? ");
+            telefono=Console.ReadLine();
+
+            Console.WriteLine("¿Cual es su direccion? ");
+            direccion=Console.ReadLine();
+    
+        }
+        public void Saludar()
+        {
+
+            Console.WriteLine("Hola soy: "+nombre+" mi apellido es: "+apellidos+ " mi telefono es: "+telefono+" Y mi direccion es : "+direccion+"\n");
+        }
+        class ProbarContacto:Contacto
+        {
+            static void Main (string[] args)
+            {
+                Contacto contac1=new Contacto();
+                Contacto contac2=new Contacto();
+                Console.WriteLine("Dados de contacto 1: ");
+                contac1.SetContacto();
+                contac1.Saludar();
+                Console.WriteLine("Dados de contacto 2: ");
+                contac2.SetContacto();
+                contac2.Saludar();
+                
+            }
+        }
+
+
+    }
+}
